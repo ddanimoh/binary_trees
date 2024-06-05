a0a05a0d7220ecaa289e778d7fea9f43e56ed40a
ddanimoh <ddanimoh@gmail.com>
2024-06-05 07:10:50 -0700

    cc

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -7,16 +7,25 @@
  * Return: Common ancestor node, or NULL if no common ancestor was found
  */
 binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
-                const binary_tree_t *second)
+		const binary_tree_t *second)
 {
-        const binary_tree_t *a, *b;
+	const binary_tree_t *a, *b;
 
-        if (first == NULL || second == NULL)
-                return (NULL);
+	if (first == NULL || second == NULL)
+		return (NULL);
 
-        /* If one of the nodes is the root, then root is the LCA */
-        if (first == second)
-                return ((binary_tree_t *)first);
+	/* If one of the nodes is the root, then root is the LCA */
+	if (first == second)
+		return ((binary_tree_t *)first);
 
-        /* Move up the tree until we find a common ancestor */
-        a = first, b = second;
+	/* Move up the tree until we find a common ancestor */
+	a = first;
+	b = second;
+
+	while (a != b)
+	{
+		a = a->parent == NULL ? second : a->parent;
+		b = b->parent == NULL ? first : b->parent;
+	}
+	return ((binary_tree_t *)a);
+}
