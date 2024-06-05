5fde767f7b2a90bb70ee47911198c9aaabc38744
ddanimoh <ddanimoh@gmail.com>
2024-06-05 06:54:49 -0700

    fot

---
+++ 100-binary_trees_ancestor.c
@@ -0,0 +1,22 @@
+#include "binary_trees.h"
+/**
+ * binary_trees_ancestor - Finds lowest common ancestor of two nodes
+ * @first: The first node
+ * @second: The second node
+ *
+ * Return: Common ancestor node, or NULL if no common ancestor was found
+ */
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
+                const binary_tree_t *second)
+{
+        const binary_tree_t *a, *b;
+
+        if (first == NULL || second == NULL)
+                return (NULL);
+
+        /* If one of the nodes is the root, then root is the LCA */
+        if (first == second)
+                return ((binary_tree_t *)first);
+
+        /* Move up the tree until we find a common ancestor */
+        a = first, b = second;
